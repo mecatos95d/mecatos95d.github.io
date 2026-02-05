982197ba2a333fc0784578d5203b1084535604cf
Wookjae Byun (TOZ57) <mecatos95d@kaist.ac.kr>
2026-02-06 08:31:12 +0900

    js.js 업데이트

--- uibus/js/js.js
+++ uibus/js/js.js
@@ -2,7 +2,7 @@
 export const JS_VEHICLE_INFO = {
     2049: {
         company: "골든벨여행사",
-        route: "8155+",
+        route: "8155(우등)",
         year: "",
         model: "우등, Wi-Fi",
         additionalMemo: "",
